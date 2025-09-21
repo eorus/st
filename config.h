e833cc2104302ad4a6e0e740bda2cce939beaec5
Alper Orus <alperor@gmail.com>
2025-09-21 13:42:12 +0300

    first commit

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "JetBrains Mono:size=12:antialias=true:autohint=true";
+static char *font2[] = { "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -112,23 +112,24 @@ float alphaOffset = 0.0;
 float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
+
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+    "#282A2E", /* black   */
+    "#FF6C6B", /* red     */
+    "#98BE65", /* green   */
+    "#ECBE7B", /* yellow  */
+    "#51AFEF", /* blue    */
+    "#C678DD", /* magenta */
+    "#46D9FF", /* cyan    */
+    "#C5C8C6", /* white   */
+    "#5B6268", /* brblack */
+    "#EC5F67", /* brred   */
+    "#A9B665", /* brgreen */
+    "#F0C674", /* bryellow*/
+    "#8ABEB7", /* brblue  */
+    "#B294BB", /* brmagenta*/
+    "#81A2BE", /* brcyan  */
+    "#DFDFDF", /* brwhite */
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
@@ -554,4 +555,3 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
-
